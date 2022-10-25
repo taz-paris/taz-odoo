e80c8b3111d2288dc6350e638e53fd2c9476d27a
Ubuntu <ubuntu@vps-338a25b8.vps.ovh.net>
2022-10-25 23:46:09 +0200

    Actions commerciales

--- taz-common/__manifest__.py
+++ taz-common/__manifest__.py
@@ -22,7 +22,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/res_partner.xml',
+        'views/business_action.xml',
     ],
 }
