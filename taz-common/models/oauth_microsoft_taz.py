99f747e48d29332a505d494d909c9f694a0e2521
Ubuntu <ubuntu@vps-338a25b8.vps.ovh.net>
2022-10-26 16:38:25 +0200

    OAuth

---
+++ taz-common/models/oauth_microsoft_taz.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import json
+
+import requests
+import werkzeug.http
+
+from odoo import api, fields, models
+from odoo.exceptions import AccessDenied, UserError
+from odoo.addons.auth_signup.models.res_users import SignupError
+
+from odoo.addons import base
+base.models.res_users.USER_PRIVATE_FIELDS.append('oauth_access_token')
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    @api.model
+    def _auth_oauth_validate(self, provider, access_token):
+        """ return the validation data corresponding to the access token """
+        oauth_provider = self.env['auth.oauth.provider'].browse(provider)
+        validation = {} #self._auth_oauth_rpc(oauth_provider.validation_endpoint, access_token) #ADU
+        if validation.get("error"):
+            raise Exception(validation['error'])
+        if oauth_provider.data_endpoint:
+            data = self._auth_oauth_rpc(oauth_provider.data_endpoint, access_token)
+            validation.update(data)
+        # unify subject key, pop all possible and get most sensible. When this
+        # is reworked, BC should be dropped and only the `sub` key should be
+        # used (here, in _generate_signup_values, and in _auth_oauth_signin)
+        subject = next(filter(None, [
+            validation.pop(key, None)
+            for key in [
+                'sub', # standard
+                'id', # google v1 userinfo, facebook opengraph
+                'user_id', # google tokeninfo, odoo (tokeninfo)
+            ]
+        ]), None)
+        if not subject:
+            raise AccessDenied('Missing subject identity')
+        validation['user_id'] = subject
+
+        return validation
+
+    @api.model
+    def _generate_signup_values(self, provider, validation, params):
+        oauth_uid = validation['user_id']
+        #email = validation.get('email', 'provider_%s_user_%s' % (provider, oauth_uid))
+        email = validation.get('mail', 'provider_%s_user_%s' % (provider, oauth_uid)) #ADU
+        #name = validation.get('name', email)
+        name = validation.get('displayName', email) #ADU
+        return {
+            'name': name,
+            'login': email,
+            'email': email,
+            'oauth_provider_id': provider,
+            'oauth_uid': oauth_uid,
+            'oauth_access_token': params['access_token'],
+            'active': True,
+        }
+
+    @api.model
+    def _auth_oauth_signin(self, provider, validation, params):
+        """ retrieve and sign in the user corresponding to provider and validated access token
+            :param provider: oauth provider id (int)
+            :param validation: result of validation of access token (dict)
+            :param params: oauth parameters (dict)
+            :return: user login (str)
+            :raise: AccessDenied if signin failed
+
+            This method can be overridden to add alternative signin methods.
+        """
+        oauth_uid = validation['user_id']
+        _logger.info(validation)
+        try:
+            oauth_user = self.search([("oauth_uid", "=", oauth_uid), ('oauth_provider_id', '=', provider)])
+            if not oauth_user:#ADU
+                oauth_user = self.search([("email", "=", validation['mail'])]) #ADU
+            if not oauth_user:
+                raise AccessDenied()
+            assert len(oauth_user) == 1
+            oauth_user.write({'oauth_access_token': params['access_token']})
+            return oauth_user.login
+        except AccessDenied as access_denied_exception:
+            if self.env.context.get('no_user_creation'):
+                return None
+            state = json.loads(params['state'])
+            token = state.get('t')
+            values = self._generate_signup_values(provider, validation, params)
+            try:
+                login, _ = self.signup(values, token)
+                return login
+            except (SignupError, UserError):
+                raise access_denied_exception
+
