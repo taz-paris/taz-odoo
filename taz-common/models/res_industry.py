0f734917b41afa2c8fe479e7da425c2a5cb8766d
Ubuntu <ubuntu@vps-338a25b8.vps.ovh.net>
2022-11-21 14:10:43 +0100

    Add OCA base_import_security_group module

---
+++ taz-common/models/res_industry.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError, AccessError
+from odoo import _
+
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class tazResIndustry(models.Model):
+    _inherit = "res.partner.industry"
+
+    ms_planner_plan_id = fields.Char("M$ planner plan ID", help="Id of the Microsoft Planner plan where tasks should be created for business action of that industry")
+    pillar_id = fields.Many2one('res.partner.industry.pillar', string = "Pillier")
+    user_id = fields.Many2one('res.users', string='Business Domaine Leader')
