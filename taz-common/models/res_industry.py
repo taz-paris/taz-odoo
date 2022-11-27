bb3bb7007e18abd4679606ebd18d7da623d2c568
Ubuntu <ubuntu@vps-338a25b8.vps.ovh.net>
2022-11-27 18:28:13 +0100

    Ajout de la liste des entreprises à l'objet industry

--- taz-common/models/res_industry.py
+++ taz-common/models/res_industry.py
@@ -14,3 +14,4 @@ class tazResIndustry(models.Model):
     ms_planner_plan_id = fields.Char("M$ planner plan ID", help="Id of the Microsoft Planner plan where tasks should be created for business action of that industry")
     pillar_id = fields.Many2one('res.partner.industry.pillar', string = "Pillier")
     user_id = fields.Many2one('res.users', string='Business Domaine Leader')
+    partner_ids = fields.One2many('res.partner', 'industry_id', string="Entreprises")
