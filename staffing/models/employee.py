42c530ca4bd5e9bcb9a92c2d715c7f54b1cb6dd9
Ubuntu <ubuntu@vps-338a25b8.vps.ovh.net>
2022-12-10 17:30:56 +0100

    Staffing

--- staffing/models/employee.py
+++ staffing/models/employee.py
@@ -8,4 +8,26 @@ _logger = logging.getLogger(__name__)
 class staffingEmployee(models.Model):
     _inherit = "hr.employee"
 
+    def _sync_user(self, user, employee_has_image=False):
+        _logger.info('_sync_user')
+        vals  = super()._sync_user(user, employee_has_image) 
+        _logger.info('_sync_user AFTER')
+        if user.first_name :
+            _logger.info('first_name' + str(user.first_name))
+            vals['first_name'] = user.first_name
+        return vals
+
+
     first_name = fields.Char(string="Prénom")
+
+
+
+
+
+class staffingUsers(models.Model):
+    _inherit = "res.users"
+
+    def _get_employee_fields_to_sync(self):
+        res = super()._get_employee_fields_to_sync()
+        res.append('first_name')
+        return res
