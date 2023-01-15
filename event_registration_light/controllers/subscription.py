73c2113caca24835612c7203ea38c8b9300eeb3f
Ubuntu <ubuntu@vps-338a25b8.vps.ovh.net>
2023-01-15 02:10:08 +0100

    Event subcription

--- event_registration_light/controllers/subscription.py
+++ event_registration_light/controllers/subscription.py
@@ -6,19 +6,22 @@ from odoo.osv import expression
 from odoo.tools.misc import get_lang
 #from odoo.tools import lazy
 
+import logging
+_logger = logging.getLogger(__name__)
 import babel.dates
 
 class EventRegistrationLightController(http.Controller):
 
     @http.route(['/eventlight/<model("event.event"):event>/registration/new'], type='http', auth="public", methods=['GET'])
     def registration_form(self, event):
-        #TODO - contrôle places restreintes
         #TODO - contrôle uuid de l'invitation
-        #TODO - refuser accès si l'evènement est passé
+        #TODO : envoyer un lien d'annulation par mail
         #availability_check = True
         #if event.seats_limited:
             # count registration with state = open
             # if seats_limit overpassaed => return "no seat availlable" page
+        if not event.event_registrations_open :
+            return "Les inscription ne sont pas ouvertes pour cet évènement."
         return request.env['ir.ui.view']._render_template("event_registration_light.registration_form", {
             'event': event,
             'formated_dates' : self.get_formated_date(event),
@@ -26,6 +29,41 @@ class EventRegistrationLightController(http.Controller):
 
     @http.route(['/eventlight/<model("event.event"):event>/registration/submit'], type='http', auth="public", methods=['POST'])
     def registration_submit(self, event, **post):
+        if not event.event_registrations_open : #TODO : si on veut que ça restourne False si le nombre de place max est réservé... il va falloir créer un ticket en plus de la resgitration
+            return "Les inscription ne sont pas ouvertes pour cet évènement."
+
+        if not 'email' in post.keys() or  not post['email']:
+            return "Pas d'adresse email => inscirption KO"
+        email = post['email']
+        _logger.info(email)
+        first_name = post['first_name']
+        name = post['name']
+        registrations = request.env['event.registration'].search([('event_id', '=', event.id)])
+        target = False
+        for reg in registrations :
+            if reg['email'] == email :
+                target = reg
+                break
+            if reg['partner_id']:
+                if reg['partner_id'].email == email or reg['partner_id'].personal_email == email or (reg['partner_id'].former_email_address and (email in reg['partner_id'].former_email_address)) :
+                    target = reg
+                    break
+        if target :
+            target.state = "open"
+        else :
+            partner = request.env['res.partner'].search([('email', '=', email), ('active', '=', True)])
+            if not partner :
+                partner = request.env['res.partner'].search([('personal_email', '=', email), ('active', '=', True)])
+            if not partner :
+                partner = request.env['res.partner'].search([('former_email_address', 'in', email), ('active', '=', True)])
+            if len(partner) >= 1 :
+                partner_id = partner[0].id
+            else :
+                partner_id = False
+                # TODO : est-ce qu'on le crée avec un user dédié et/ou on le top "créé suite à inscription event" ==> si un rebot nous pollue àa permettrait de les repérer
+
+            #create registration
+            target = request.env['event.registration'].create({'event_id' : event.id, 'partner_id' : partner_id, 'email' : email, 'name' : first_name + " " + name, 'state' : 'open'})
         return request.env['ir.ui.view']._render_template("event_registration_light.registration_submit")
 
 
--- odoo-rest-api/__manifest__.py
+++ odoo-rest-api/__manifest__.py
@@ -11,6 +11,7 @@
 
     'author': "Yezileli Ilomo",
     'website': "https://github.com/yezyilomo/odoo-rest-api",
+    'license': 'LGPL-3',
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
