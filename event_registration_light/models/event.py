fe57331f33318e0d9c576997f04e792790118f05
Ubuntu <ubuntu@vps-338a25b8.vps.ovh.net>
2023-04-12 14:39:45 +0200

    Evenement - remove the 3 email added by default creating an event

--- event_registration_light/models/event.py
+++ event_registration_light/models/event.py
@@ -22,3 +22,33 @@ class event(models.Model):
 
     registration_form_url = fields.Char("URL du formulaire d'inscription", compute=_get_registration_form_url)
     description_web_form = fields.Html("Bloc HTML affiché sur le formulaire d'inscription")
+
+
+class EventType(models.Model):
+    _inherit = 'event.type'
+
+    def _default_event_mail_type_ids(self):
+        return []
+        """
+        return [(0, 0,
+                 {'notification_type': 'mail',
+                  'interval_nbr': 0,
+                  'interval_unit': 'now',
+                  'interval_type': 'after_sub',
+                  'template_ref': 'mail.template, %i' % self.env.ref('event.event_subscription').id,
+                 }),
+                (0, 0,
+                 {'notification_type': 'mail',
+                  'interval_nbr': 1,
+                  'interval_unit': 'hours',
+                  'interval_type': 'before_event',
+                  'template_ref': 'mail.template, %i' % self.env.ref('event.event_reminder').id,
+                 }),
+                (0, 0,
+                 {'notification_type': 'mail',
+                  'interval_nbr': 3,
+                  'interval_unit': 'days',
+                  'interval_type': 'before_event',
+                  'template_ref': 'mail.template, %i' % self.env.ref('event.event_reminder').id,
+                 })]
+        """
