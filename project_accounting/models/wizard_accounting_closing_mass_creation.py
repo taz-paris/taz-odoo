f28d1bd88d3af07b95708eb273a462bf4865816b
Ubuntu <ubuntu@vps-338a25b8.vps.ovh.net>
2023-09-02 19:00:08 +0200

    Add wizard_accounting_closing_mass_creation

---
+++ project_accounting/models/wizard_accounting_closing_mass_creation.py
@@ -0,0 +1,41 @@
+from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError, AccessError
+from odoo import _
+import datetime
+from dateutil.relativedelta import relativedelta
+
+import logging
+_logger = logging.getLogger(__name__)
+
+    
+class wizardAccountingClosingMassCreation(models.TransientModel):
+    _name = 'wizard_accounting_closing_mass_creation'
+    _description = "wizard_accounting_closing_mass_creation"
+
+
+    @api.onchange('date')
+    def _onchange_date(self):
+        self.project_ids = False
+        p = self.env['project.project'].search([('stage_id.state', 'in', ['before_launch', 'launched'])])
+        #TODO : il faudrait mieux les projets non cloturés à cette DATE ou bien cloturer entre la DATE et le pointage précédent
+        self.project_ids = p.ids
+        self.project_ids = [1129] #projet test3.1
+
+    def _default_date(self):
+        return datetime.date.today().replace(day=1) - datetime.timedelta(1) 
+
+
+
+    date = fields.Date('Date de clôture', default=_default_date)
+    project_ids = fields.Many2many('project.project')
+
+
+
+    def action_validate(self):
+        for project_id in self.project_ids:
+            closing_ids = self.env['project.accounting_closing'].search([('project_id', '=', project_id.id), ('closing_date', '=', self.date)])
+            if len(closing_ids) == 0:
+                self.env['project.accounting_closing'].create({
+                        'project_id' : project_id.id,
+                        'closing_date' : self.date,
+                    })
