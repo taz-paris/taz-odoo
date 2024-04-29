4f730b577cf6b07a44d819e81b4d06a9aba680c9
Ubuntu <ubuntu@vps-338a25b8.vps.ovh.net>
2024-04-29 14:26:46 +0200

    debug

--- napta_connector/data/neutralize.sql
+++ napta_connector/data/neutralize.sql
@@ -1,3 +1,3 @@
 -- desactivate napta connexion parameters
 DELETE FROM ir_config_parameter
-    WHERE key IN ('napta_client_id', 'napta_client_secret');
+    WHERE key IN ('napta_client_id', 'napta_client_secret', 'napta_access_values');
