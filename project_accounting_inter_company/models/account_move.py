8a2d27c3376830855947f4006000da55ec797e91
yuntux <yuntux@gmail.com>
2024-08-14 17:01:30 +0200

    inter company debug

--- project_accounting_inter_company/models/account_move.py
+++ project_accounting_inter_company/models/account_move.py
@@ -14,6 +14,10 @@ class AccountMoveLine(models.Model):
     def _prepare_account_move_line(self, dest_move, dest_company):
         _logger.info('==== _prepare_account_move_line')
         new_line = super()._prepare_account_move_line(dest_move, dest_company)
-        new_line['analytic_distribution'] = self.env['purchase.order'].get_dest_analytic_distribution(self.analytic_distribution, dest_company)
-        _logger.info(new_line)
+        if dest_move.move_type in ['in_invoice', 'in_refund']:
+            new_line['analytic_distribution'] = self.env['purchase.order'].get_dest_analytic_distribution_from_supplier_company(self.analytic_distribution, dest_company, self.company_id)
+        elif dest_move.move_type in ['out_invoice', 'out_refund']:
+            new_line['analytic_distribution'] = self.env['purchase.order'].get_dest_analytic_distribution(self.analytic_distribution, dest_company)
+        else :
+            raise ValidationError(_("Type d'écriture non géré par le module project_accounting_inter_company."))
         return new_line
