8b79289697e1647d7f7e7a0c659d257281b6cddd
Ubuntu <ubuntu@vps-338a25b8.vps.ovh.net>
2024-08-14 10:47:15 +0200

    inter company

--- project_accounting_inter_company/models/purchase_order.py
+++ project_accounting_inter_company/models/purchase_order.py
@@ -14,19 +14,20 @@ class PurchaseOrder(models.Model):
     def get_dest_analytic_distribution(self, analytic_distribution, dest_company):
         _logger.info('=================  get_dest_analytic_distribution')
         dest_analytic_distribution = {}
+        
+        if analytic_distribution:
+            for analytic_account_id, rate in analytic_distribution.items() :
+                analytic_account_id = self.env['account.analytic.account'].browse([int(analytic_account_id)])
 
-        for analytic_account_id, rate in analytic_distribution.items() :
-            analytic_account_id = self.env['account.analytic.account'].browse([int(analytic_account_id)])
-
-            if analytic_account_id.project_count != 1:
-                raise ValidationError(_("Ce compte analytique ne peut pas être traité automatiquement car il n'est pas lié à exactement un projet."))
-            project_id = analytic_account_id.project_ids[0] 
-            project_outsourcing_link_ids = self.env['project.outsourcing.link'].search([('project_id', '=', project_id.id), ('partner_id', '=', dest_company.partner_id.id)])
-            if len(project_outsourcing_link_ids) != 1:
-                raise ValidationError(_("Le projet %s n'a pas de lien projet/sous-traitant avec la société %s" % (project_id.name_get(), dest_company.partner_id.name())))
-            project_outsourcing_link_id = project_outsourcing_link_ids[0]
-            dest_project = project_outsourcing_link_id.get_or_generate_inter_company_mirror_project()
-            dest_analytic_distribution[str(dest_project.analytic_account_id.id)] = rate
+                if analytic_account_id.project_count != 1:
+                    raise ValidationError(_("Ce compte analytique ne peut pas être traité automatiquement car il n'est pas lié à exactement un projet."))
+                project_id = analytic_account_id.project_ids[0] 
+                project_outsourcing_link_ids = self.env['project.outsourcing.link'].search([('project_id', '=', project_id.id), ('partner_id', '=', dest_company.partner_id.id)])
+                if len(project_outsourcing_link_ids) != 1:
+                    raise ValidationError(_("Le projet %s n'a pas de lien projet/sous-traitant avec la société %s" % (project_id.name_get(), dest_company.partner_id.name())))
+                project_outsourcing_link_id = project_outsourcing_link_ids[0]
+                dest_project = project_outsourcing_link_id.get_or_generate_inter_company_mirror_project()
+                dest_analytic_distribution[str(dest_project.analytic_account_id.id)] = rate
 
         return dest_analytic_distribution
 
