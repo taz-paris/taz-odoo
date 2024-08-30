b7ba3cabe618c3eac773aed0b32c8bd39ebdfaac
Ubuntu <ubuntu@vps-338a25b8.vps.ovh.net>
2024-08-30 17:29:14 +0200

    Improvments

--- dashboard_global/controllers/outsourcing_dashboard.py
+++ dashboard_global/controllers/outsourcing_dashboard.py
@@ -22,37 +22,52 @@ class OutsourcingDashboardController(http.Controller):
     def get_data_dict(self, company_id, partner_id):
         data = {}
         projects = {}
-        project_outsourcing_link_ids = request.env['project.outsourcing.link'].search([('company_id', '=', company_id), ('partner_id', '=', partner_id)])
+        project_outsourcing_link_ids = request.env['project.outsourcing.link'].search([('company_id', '=', company_id), ('partner_id', '=', partner_id)], order="project_id desc")
         for pol in project_outsourcing_link_ids:
             p = pol.project_id
-            if p.sudo().stage_id.id in [3, 8, 4]:
+            if p.sudo().stage_id.id in [8, 4]: #8 = Perdu / 4 = Annulé
                 continue
-            projects[p.id] = p.sudo().read(['partner_id', 'stage_id', 'number', 'name', 'project_director_employee_id', 'date_start', 'date'])[0]
+            projects[p.id] = p.sudo().read(['partner_id', 'stage_id', 'number', 'name', 'project_director_employee_id', 'date_start', 'date', 'remark', 'invoicing_comment'])[0]
 
 
             projects[p.id]['sale_order_lines'] = []
             sale_order_line_ids = p.sudo().get_sale_order_line_ids()
             sale_order_lines = request.env['sale.order.line'].sudo().browse(sale_order_line_ids)
+            total_sale_order_lines = {'name' : '', 'order_id' : ('', 'TOTAL'), 'price_unit' : '', 'product_uom_qty' : '', 'price_subtotal' : 0.0, 'previsional_invoice_date' : '', 'comment' : '', 'invoice_lines' : []}
+            total_invoice_lines = {'name' : '', 'ref' : 'TOTAL', 'date' : '', 'price_unit' : '', 'quantity' : '', 'price_subtotal_signed' : 0.0, 'parent_payment_state' : ''}
             for sale_order_line in sale_order_lines:
                 d = sale_order_line.sudo().read(['name', 'order_id', 'price_unit', 'product_uom_qty', 'price_subtotal', 'previsional_invoice_date', 'comment'])[0]
+                total_sale_order_lines['price_subtotal'] += d['price_subtotal']
                 d['invoice_lines'] = []
                 for il in sale_order_line.sudo().invoice_lines :
                     if il.parent_state != 'cancel':
-                        d['invoice_lines'].append(il.sudo().read(['name', 'ref', 'date', 'price_unit', 'quantity', 'price_subtotal_signed', 'parent_payment_state'])[0])
+                        inv_line = il.sudo().read(['name', 'ref', 'date', 'price_unit', 'quantity', 'price_subtotal_signed', 'parent_payment_state'])[0]
+                        d['invoice_lines'].append(inv_line)
+                        total_invoice_lines['price_subtotal_signed'] += inv_line['price_subtotal_signed']
                 projects[p.id]['sale_order_lines'].append(d)
+            
+            projects[p.id]['sale_order_lines'].append(total_sale_order_lines)
+            projects[p.id]['sale_order_lines'][len(projects[p.id]['sale_order_lines'])-1]['invoice_lines'].append(total_invoice_lines)
 
 
             projects[p.id]['purchase_order_lines'] = []
             purchase_order_line_ids = pol.sudo().get_purchase_order_line_ids() 
             purchase_order_lines = request.env['purchase.order.line'].sudo().browse(purchase_order_line_ids)
+            total_purchase_order_lines = {'name' : '', 'order_id' : ('', 'TOTAL'), 'price_unit' : '', 'product_uom_qty' : '', 'price_subtotal' : 0.0, 'previsional_invoice_date' : '', 'state' : '', 'invoice_lines' : []}
+            total_invoice_lines = {'name' : '', 'ref' : 'TOTAL', 'date' : '', 'price_unit' : '', 'quantity' : '', 'price_subtotal_signed' : 0.0, 'parent_payment_state' : ''}
             for purchase_order_line in purchase_order_lines:
                 d = purchase_order_line.read(['name', 'order_id', 'price_unit', 'product_uom_qty', 'price_subtotal', 'previsional_invoice_date', 'state'])[0]
+                total_purchase_order_lines['price_subtotal'] += d['price_subtotal']
                 d['invoice_lines'] = []
                 for il in purchase_order_line.sudo().invoice_lines :
                     if il.parent_state != 'cancel':
-                        d['invoice_lines'].append(il.sudo().read(['name', 'ref', 'date', 'price_unit', 'quantity', 'price_subtotal_signed', 'parent_payment_state'])[0])
+                        inv_line = il.sudo().read(['name', 'ref', 'date', 'price_unit', 'quantity', 'price_subtotal_signed', 'parent_payment_state'])[0]
+                        d['invoice_lines'].append(inv_line)
+                        total_invoice_lines['price_subtotal_signed'] += inv_line['price_subtotal_signed']
                 projects[p.id]['purchase_order_lines'].append(d)
 
+            projects[p.id]['purchase_order_lines'].append(total_purchase_order_lines)
+            projects[p.id]['purchase_order_lines'][len(projects[p.id]['purchase_order_lines'])-1]['invoice_lines'].append(total_invoice_lines)
             
 
         for pid, pval in projects.items():
