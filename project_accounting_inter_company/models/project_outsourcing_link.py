0bc2d62ca912a22ccbcded920284fe3f3b68ff76
Ubuntu <ubuntu@vps-338a25b8.vps.ovh.net>
2024-10-08 00:29:25 +0200

    imrove intercompany

--- project_accounting_inter_company/models/project_outsourcing_link.py
+++ project_accounting_inter_company/models/project_outsourcing_link.py
@@ -11,14 +11,24 @@ _logger = logging.getLogger(__name__)
 class projectOutsourcingLink(models.Model):
     _inherit = "project.outsourcing.link"
 
-    inter_company_mirror_project = fields.Many2one('project.project', 'Projet miroir')
+    inter_company_mirror_project = fields.Many2one('project.project', 'Projet miroir', check_company=False, help="Le projet miroir est créé automatiquement lors de la validation du premier BCF ou de la première facture.\n S'il existe déjà projet miroir dans le référentiel des projets de la filiale fournisseuse, il est possible de le renseigner à la main.")
+
+    @api.depends('partner_id', 'partner_id.ref_company_ids')
+    def _compute_is_partner_id_res_company(self):
+        for rec in self:
+            if len(rec.partner_id.ref_company_ids) != 1:
+                rec.is_partner_id_res_company = False
+            else :
+                rec.is_partner_id_res_company = True
+
+    is_partner_id_res_company = fields.Boolean(compute="_compute_is_partner_id_res_company")
 
     def get_or_generate_inter_company_mirror_project(self) :
         _logger.info('==== get_or_generate_inter_company_mirror_project')
         self.ensure_one()
         if self.inter_company_mirror_project:
             return self.inter_company_mirror_project
-        if len(self.partner_id.ref_company_ids) != 1:
+        if not self.is_partner_id_res_company :
             raise ValidationError(_("Le partenaire lié n'est pas lié à une société de la galaxie."))
         
         dic_mirror_project = {
