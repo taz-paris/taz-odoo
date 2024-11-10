8ddfdcea1c6b3a20b218644cdc9e900f175a327e
Ubuntu <ubuntu@vps-338a25b8.vps.ovh.net>
2024-11-10 01:22:58 +0100

    Miror project

--- project_accounting_inter_company/models/project.py
+++ project_accounting_inter_company/models/project.py
@@ -10,7 +10,19 @@ _logger = logging.getLogger(__name__)
 
 class projectProject(models.Model):
     _inherit = "project.project"
-    
+   
+    def compute_is_related_to_miror_project(self):
+        for rec in self :
+            is_related_to_miror_project = False
+            if len(rec.mirored_project_outsourcing_link_ids) != 0 :
+                is_related_to_miror_project = True
+            for project_outsourcing_link_id in rec.project_outsourcing_link_ids:
+                if project_outsourcing_link_id.inter_company_mirror_project :
+                    is_related_to_miror_project = True
+            rec.is_related_to_miror_project = is_related_to_miror_project
+
+
+    is_related_to_miror_project = fields.Boolean(compute = compute_is_related_to_miror_project)
     mirored_project_outsourcing_link_ids = fields.One2many('project.outsourcing.link', 
                                                             'inter_company_mirror_project', 
                                                             check_company=False,
