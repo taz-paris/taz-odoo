a809c59c820c1d8e0a946a4b67603e0bb8316399
Aurélien Dumaine <yuntux@gmail.com>
2025-06-27 22:39:06 +0000

    [17.0][MIG] Debug spreadsheet_filters

--- spreadsheet_filters/static/src/helpers.js
+++ spreadsheet_filters/static/src/helpers.js
@@ -9,21 +9,15 @@ import { serializeDate, serializeDateTime } from "@web/core/l10n/dates";
 
 
 export function getRelativeDateDomainNewFilters(now, offset, rangeType, fieldName, fieldType) {
-        //console.log("========= Fonction getRelativeDateDomainNewFilter");
-        //console.log(rangeType);
+    //console.log("========= Fonction getRelativeDateDomainNewFilter");
+    //console.log(rangeType);
     let endDate = now.minus({ day: 1 }).endOf("day");
     let startDate = endDate;
     switch (rangeType) {
-        case "year_to_date": { //year_to_date already exists in Odoo core 17.0 and should be remove from here when migrating this module **IF the serializeDateTime issue beelow is corrected**
-            const offsetParam = { years: offset };
-            startDate = now.startOf("year").plus(offsetParam);
-            endDate = now.endOf("day").plus(offsetParam);
-            break;
-        }
         case "year_to_last_closed_month": {
             const offsetParam = { years: offset };
             startDate = now.startOf("year").plus(offsetParam);
-            endDate = now.minus({months: 1}).endOf("day").plus(offsetParam);
+            endDate = now.minus({months: 1}).endOf("month").plus(offsetParam);
             break;
         }
         case "year_to_last_closed_week": {
@@ -48,41 +42,34 @@ export function getRelativeDateDomainNewFilters(now, offset, rangeType, fieldNam
             const offsetParam = { years: offset };
             startDate = now.minus({year:1000}).startOf("year");
             endDate = now.endOf("day").plus(offsetParam);
-	console.log(startDate);
-	console.log(endDate);
             break;
         }
         case "today_future": {
             const offsetParam = { years: offset };
             startDate = now.startOf("day").plus(offsetParam);
             endDate = now.plus({year:1000}).endOf("year");
-	console.log(startDate);
-	console.log(endDate);
             break;
         }
         default:{
             return undefined;
         }
     }
-    startDate = startDate.startOf("day");
 
     let leftBound, rightBound;
+
+    console.log(startDate);
+    console.log(endDate);
     if (fieldType === "date") {
 	    console.log("serializeDate");
         leftBound = serializeDate(startDate);
         rightBound = serializeDate(endDate);
     } else {
-	    console.log("serializeDateTime");
-	//leftBound = serializeDateTime(startDate);
-        //rightBound = serializeDateTime(endDate);
-	// BUG du core ? Le fonction serializeDateTime from "@web/core/l10n/dates" force le passage à UTC : setZone("utc").
-	    // Ce qui conduit à remonter les clotures comptables du 31/12/N-1 puisque leftBound est le 31/12 à 23h en UTC.
-	const SERVER_DATE_FORMAT = "yyyy-MM-dd";
-	const SERVER_TIME_FORMAT = "HH:mm:ss";
-	const SERVER_DATETIME_FORMAT = `${SERVER_DATE_FORMAT} ${SERVER_TIME_FORMAT}`;
-	leftBound = startDate.toFormat(SERVER_DATETIME_FORMAT, { numberingSystem: "latn" })
-	rightBound = endDate.toFormat(SERVER_DATETIME_FORMAT, { numberingSystem: "latn" })
+	//console.log("serializeDateTime");
+        leftBound = serializeDateTime(startDate);
+        rightBound = serializeDateTime(endDate);
     }
+    console.log(leftBound);
+    console.log(rightBound);
 
     return new Domain(["&", [fieldName, ">=", leftBound], [fieldName, "<=", rightBound]]);
 }
@@ -102,7 +89,7 @@ patch(GlobalFiltersUIPlugin.prototype, {
 		const now = DateTime.local();
 		
 		if (filter.rangeType === "relative") {
-	    	if (["year_to_date", "year_to_last_closed_month", "year_to_last_closed_week", "current_week_to_end_year", "current_year", "past_until_today", "today_future"].includes(value)){
+	    	if (["year_to_last_closed_month", "year_to_last_closed_week", "current_week_to_end_year", "current_year", "past_until_today", "today_future"].includes(value)){
 				return getRelativeDateDomainNewFilters(now, offset, value, field, type);
 			}
      	}    	
