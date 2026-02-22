ff2193cec544f9cfa9b44508fce15891b567e045
yuntux <yuntux@gmail.com>
2026-02-22 18:09:24 +0100

    [CLEAN] taz-common: suppression du niveau de priorité de compte ‘inditto’
    Nettoyage du code et des vues suite à la suppression du niveau de priorité
    ‘Contribution Inditto’ dans la gestion des comptes.
    
    - Modèle res.partner.industry : retrait de l'option 'inditto' dans la
      Selection du champ business_priority.
    - Vues (industry, partner, goal, business_action) : suppression de tous
      les filtres de recherche basés sur la valeur ‘inditto’.

--- taz-common/models/res_industry.py
+++ taz-common/models/res_industry.py
@@ -24,7 +24,6 @@ class tazResIndustry(models.Model):
          ('active', '1-Compte actif'),
          ('priority_target', '2-Compte prioritaire'),
          ('not_tracked', '3-Opportunités'),
-         ('inditto', '4-Contribution Inditto'),
     ], "Niveau de priorité", default='not_tracked')#, tracking=True)
 
     customer_book_goal_ids = fields.One2many('taz.customer_book_goal', 'industry_id')  
